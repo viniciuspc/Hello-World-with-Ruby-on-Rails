f16194592a9a99b4ad0c2618c24937d8f192c073
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-10 11:24:20 +0100

    Scaffold Articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   root "pages#home"
   #GET request to about page will be handle by the pages#about controller
   get "about", to: "pages#about"
