9e9eebda2dc52e9b014680f1308d5798e629d4bb
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-15 09:23:06 +0100

    Create user and relate it with articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: {minimun: 6, maximum: 100}
+  validates :description, presence: true, length: {minimun: 10, maximum: 300}
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :articles
   root "pages#home"
   #GET request to about page will be handle by the pages#about controller
